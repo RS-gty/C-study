6a4378f670168ae2a4ac7b815fac84e69ee0e5e0
RS-gty <18920702372@163.com>
2025-11-13 23:54:26 +0800

    string utils

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Basic type definition
 typedef struct ex{
@@ -17,6 +18,32 @@ int get_length(const char* string) {
         length++;
     }
     return length;
+}
+
+char** sep_by(const char* string, char separator) {
+    int length = get_length(string);
+    char part1[length], part2[length];
+    int index1=0, index2=0;
+    int trigger = 0;
+    for (int i = 0; i < length; i++) {
+        if (string[i] != separator || trigger == 1) {
+            if (!trigger) {
+                part1[index1] = string[i];
+                index1 ++;
+            } else {
+                part1[index1] = '\0';
+                part2[index2] = string[i];
+                index2 ++;
+            }
+        } else {
+            trigger = 1;
+        }
+    }
+    part2[index2] = '\0';
+    char **result = (char**)malloc(sizeof(char) * length * 2);
+    result[0] = part1;
+    result[1] = part2;
+    return result;
 }
 
 void replace(char* string, int begin_index, int end_index, const char* new_content) {
@@ -57,6 +84,37 @@ int is_number(const char* string) {
     }
     return 1;
 }
+
+int is_pow_of_a(const char* string) {
+    char **arr = sep_by(string, '^');
+    if (!(get_length(arr[0])* get_length(arr[1]))) {
+        free(arr);
+        return 0;
+    }
+    if (arr[0] == "a" && is_number(arr[1])) {
+        free(arr);
+        return 1;
+    }
+    return 0;
+}
+
+int is_num_times_a(const char* string) {
+    char **arr = sep_by(string, '*');
+    if (!(get_length(arr[0])* get_length(arr[1]))) {
+        free(arr);
+        return 0;
+    }
+    if (is_number(arr[0]) && is_pow_of_a(arr[1])) {
+        free(arr);
+        return 1;
+    }
+    if (is_pow_of_a(arr[0]) && is_number(arr[1])) {
+        free(arr);
+        return 1;
+    }
+    return 0;
+}
+
 
 int main() {
     char string[1000] = "((a^1) ^ 2)^3";
